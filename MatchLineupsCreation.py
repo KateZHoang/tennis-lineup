f02e80ecd9ebdffab553059c2846c5672826d99c
kate <kate.z.hoang@gmail.com>
2024-11-21 15:19:10 -0800

    Fix IndexError

--- MatchLineupsCreation.py
+++ MatchLineupsCreation.py
@@ -62,7 +62,7 @@ def generate_lineups(players, sets=3):
         
         matches = []
 
-        for i in range(0, len(pairs)):
+        for i in range(0, len(pairs), 2) and len(pairs) >= 2:
             temp_match = tuple(sorted([pairs[i], pairs[i + 1]]))
 
             # Only add the matchup if not already exist

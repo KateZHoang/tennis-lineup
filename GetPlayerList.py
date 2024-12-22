5e470334cdab66362b01f56698720b02d734be86
kate <kate.z.hoang@gmail.com>
2024-12-21 23:49:58 -0800

    Resolve merge conflict. Updated player attribute to allow comparison

--- GetPlayerList.py
+++ GetPlayerList.py
@@ -10,24 +10,10 @@ class Player:
     
     def __repr__(self):
         return f"Player({self.name}, {self.gender}, {self.level})"
-<<<<<<< HEAD
 
     def __lt__(self, other):
         # Compare players primarily by name, alphabetically
         return self.name < other.name
-=======
-'''
-# Set up the scope and authenticate
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-creds = Credentials.from_service_account_file("/Users/ItsKate/Downloads/googlesheetapi.json", scopes=scope)
-client = gspread.authorize(creds)
-
-# Access your Google Sheet
-sheet = client.open_by_key("1Z3jSqZv4cikdgrajeFmP5Dd1O9NOt_f1WWFkUCCkMOo").worksheet("Test")  # or specify the sheet by name
-'''
->>>>>>> 17f25368a85e4f2164d4d445e02438c87f1bdc39
 
 # Create player list from Google Sheet
 def get_players(sheet):
@@ -42,21 +28,3 @@ def get_players(sheet):
             player_list.append(player)
 
     return player_list
-<<<<<<< HEAD
-=======
-
-'''
-OLD CODE
-
-# Get data as a list of lists or dictionaries
-data = sheet.get_all_records()  # list of dictionaries
-rows_as_lists = [list(row.values()) for row in data]  # convert each row to list if needed
-
-player_list = []
-for i in rows_as_lists:
-    if i[3] == 'Yes':
-        player = Player(i[0], i[1], i[2])
-        player_list.append(player)
-
-'''
->>>>>>> 17f25368a85e4f2164d4d445e02438c87f1bdc39

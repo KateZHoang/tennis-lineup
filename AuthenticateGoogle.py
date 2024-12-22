aa26e6ca81a9c1e1a3034ffa79b9a58a608b5d1d
kate <kate.z.hoang@gmail.com>
2024-12-21 22:17:17 -0800

    Resolved merge conflicts

---
+++ AuthenticateGoogle.py
@@ -0,0 +1,27 @@
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+from dotenv import load_dotenv
+
+def authenticate_google():
+    load_dotenv()  # Load environment variables from .env file
+
+    # Load service account key from environment variable
+    secret_key = os.getenv("SERVICE_ACCOUNT_KEY")
+
+    if not secret_key:
+        raise ValueError("SERVICE_ACCOUNT_KEY environment variable is not set.")
+
+    # Parse the JSON string
+    parsed_json = json.loads(secret_key)
+
+    # Authenticate with Google API using the JSON secret
+    scope = ["https://spreadsheets.google.com/feeds", 
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file", 
+            "https://www.googleapis.com/auth/drive"
+    ]
+
+    # return the parsed_json and scope to be used if needed
+    return parsed_json, scope
--- UpdateGSheet.py
+++ UpdateGSheet.py
@@ -57,3 +57,4 @@ lineups = MatchLineupsCreation.generate_lineups(players, sets = 3)
 data = reformat_lineup(lineups)
 print(data)
 write_to_gsheet(data)
+

093aedad2c12a09c3733521f75d4638234a47311
kate <kate.z.hoang@gmail.com>
2025-06-21 23:26:59 -0700

    add conftest.py to support testing with dynamic data source mock vs real

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from mock_player_data import get_mock_players
+from GetPlayerList import get_players
+import AuthenticateGoogle
+
+def pytest_addoption(parser):
+    parser.addoption("--data-source", action="store", default="mock", help="mock or real")
+    parser.addoption("--mock-scenario", action="store", default="valid", help="choose mock scenario")
+
+@pytest.fixture
+def player_data(request):
+    source = request.config.getoption("--data-source")
+    if source == "real":
+        creds, client = AuthenticateGoogle.authenticate_google()
+        sheet = client.open_by_key("1pG6MNE5WRD9IikzX66HsNfme1DRZsMDVY0FUPnSnFtY").worksheet("Player_Info")
+        return get_players(sheet)
+    else:
+        scenario = request.config.getoption("--mock-scenario")
+        return get_mock_players(scenario)
